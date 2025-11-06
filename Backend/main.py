00697bff7e78b4490f90f0ad5be0e21c4e24acd3
ahmadkhatib02 <ahmad.alkhatib02@lau.edu>
2025-11-06 14:45:51 +0200

    made the endpoint a post request

--- Backend/main.py
+++ Backend/main.py
@@ -1,19 +1,58 @@
-from fastapi import FastAPI
+import os, shutil, uuid
+from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 from slidedeckai import SlideDeckAI
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 app = FastAPI()
 
-@app.get("/")
-def root():
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+OUTPUT_DIR = os.path.abspath("generated")
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+
+app.mount("/files", StaticFiles(directory=OUTPUT_DIR), name="files")
+
+class GenerateReq(BaseModel):
+    prompt: str
+
+@app.post("/api/generate")
+def generate_prompt(req: GenerateReq):
+    api_key = os.getenv("GOOGLE_API_KEY")
+    if not api_key:
+        raise HTTPException(500, "GOOGLE_API_KEY is not set")
+
     try:
         slide_generator = SlideDeckAI(
-        model='[gg]gemini-2.5-flash',
-        topic='Make a slide deck on the introduction of marvel, make it short simple',
-        api_key='AIzaSyDDyDQtYcOkCJ4J9U7ZxLuAAFGSkWaNJqw',
+            model='[gg]gemini-2.5-flash',
+            topic=req.prompt,
+            api_key=api_key,
         )
-        pptx_path = slide_generator.generate()
-        return {"status": "success",
-                "Path": pptx_path}
-    except Exception as e: 
-        return {"status": "error", 
-                "message": str(e)}
+        pptx_path = slide_generator.generate()  
+
+        
+        filename = f"{uuid.uuid4().hex}.pptx"
+        dest_path = os.path.join(OUTPUT_DIR, filename)
+        shutil.copy(pptx_path, dest_path)
+
+        
+        return {
+            "status": "success",
+            "path": f"/files/{filename}"
+        }
+
+    except Exception as e:
+        raise HTTPException(500, str(e))
